71ebb78402b603a0c4fdb55d875fa4f50da8bb4b
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-08-16 14:19:16 -0600

    feat: Se agrego la pagina para el perfil

---
+++ src/pages/profile.jsx
@@ -0,0 +1,13 @@
+
+
+export function Profile(){
+    return(
+        <>
+            <h1>Hola chum, Soy Steve</h1>
+
+            <p>
+                Ingeniero en sistemas
+            </p>
+        </>
+    )
+}
