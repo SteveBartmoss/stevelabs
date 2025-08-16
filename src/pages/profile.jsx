226bb6f55646c9fa515cb48fcb1742d099e666e3
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-08-16 15:48:34 -0600

    feat: Se agrego cotenido para el perfil

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,13 +1,27 @@
 
 
 export function Profile(){
+
+    const codeStats = 
+    `const steve={
+        lenguajes: [Javascript,php,Java,python],
+        frameWorsk: [Nests,Express,Laravel,Vue],
+        librerias: [React, Redux,Pinia,Jest],
+        dataBase: [Mysql,SlqServer,Informix],
+    }`
     return(
         <>
             <h1>Hola chum, Soy Steve</h1>
 
             <p>
-                Ingeniero en sistemas
+                Ingeniero en sistemas al que le gusta crear programas que puedan ayudar a otros o simplemente para aprender sobre algo
             </p>
+
+            <pre>
+                {
+                    codeStats
+                }        
+            </pre>
         </>
     )
 }
