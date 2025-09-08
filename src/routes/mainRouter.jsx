d8ed2d34b757bd65da66746d39897bf5610339e2
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-09-07 20:28:01 -0600

    feat: Se implemento el footer para la pagina

--- src/routes/mainRouter.jsx
+++ src/routes/mainRouter.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/home";
 import { Profile } from "../pages/profile";
 import { NavBar } from "../components/navbar/navbar";
+import { Footer } from "../components/footer/footer";
 
 
 export function MainRouter(){
@@ -12,6 +13,7 @@ export function MainRouter(){
                 <Route path="/" element={<Home />} />
                 <Route path="/profile" element={<Profile />} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     )
 }
