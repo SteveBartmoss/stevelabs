e888df2bf5e5b7ec4b103a7ee4ecb834a83df4a0
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-09-07 20:27:40 -0600

    feat: Se implementaron los estilos para el navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,10 @@
 import { NavLink } from "react-router-dom";
-
+import './navbar.css'
 
 export function NavBar(){
     return(
         <>
-            <div>
+            <div className="div-nav-bar">
                 <NavLink to={"/"}>Inicio</NavLink>
                 <NavLink to={'/proyectos'}>Proyectos</NavLink>
                 <NavLink to={'/cursos'}>Cursos</NavLink>
