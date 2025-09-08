16118ec3cfa23fc39e1e879c301719531d1813b0
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-09-07 20:35:41 -0600

    feat: Se implemento el div para las pagina principales

--- src/routes/mainRouter.jsx
+++ src/routes/mainRouter.jsx
@@ -3,16 +3,19 @@ import { Home } from "../pages/home";
 import { Profile } from "../pages/profile";
 import { NavBar } from "../components/navbar/navbar";
 import { Footer } from "../components/footer/footer";
+import { DivHome } from "../components/containers/containers";
 
 
-export function MainRouter(){
-    return(
+export function MainRouter() {
+    return (
         <BrowserRouter>
             <NavBar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/profile" element={<Profile />} />
-            </Routes>
+            <DivHome>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
+                </Routes>
+            </DivHome>
             <Footer />
         </BrowserRouter>
     )
