4a11ca43267d8284cccd943cfd311372c2e07760
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-09-08 20:48:17 -0600

    feat: Se agrego contenido para el footer de la pagina

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,13 @@ export function Footer(){
     return(
         <>
             <div>
+
                 <h1>Footer</h1>
+                <p>github</p>
+                <p>Acerca del proyecto</p>
+                <p>Blog</p>
+                <p>Contacto</p>
+                <p>Equipo</p>
             </div>
         </>
     )
