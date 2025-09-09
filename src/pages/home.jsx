8383896d6652a03e6343d2efafc28331b52da66a
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-09-08 20:48:56 -0600

    feat: Se agrego contenido para la pagina del home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,8 @@ export function Home(){
     return(
         <>
             <h1>Home</h1>
+
+            <p>Equipo de programacion dedicado a ofrecer software a la medida, opciones de software gratuito, proyectos de codigo abierto y tutoriales</p>
         </>
     )
 }
