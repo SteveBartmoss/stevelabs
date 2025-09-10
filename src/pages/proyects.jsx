14c8088fa5b0ca52d8126ac1eb4d5f46f5cdb693
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-09-09 21:15:03 -0600

    feat: Se agrego informacion para la pagina de los proyectos

---
+++ src/pages/proyects.jsx
@@ -0,0 +1,19 @@
+
+
+export function Proyecst(){
+    return(
+        <>
+            <h1>Proyectos OpenSource</h1>
+
+            <p>
+                Los siguientes proyectos tiene la licencia GNU
+            </p>
+
+            <h1>HttpClient</h1>
+
+            <h1>CronosJs</h1>
+
+            <h1>NeoFetch</h1>
+        </>
+    )
+}
