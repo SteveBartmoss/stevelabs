143c0c8a4f0a3fadab7da3f1ec2e9a21873eb2f2
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-09-16 12:49:28 -0600

    feat: Se removio el ejemplo del footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,13 +5,12 @@ export function Footer(){
         <>
             <div className='div-footer'>
 
-                <h1>Footer</h1>
                 <p>github</p>
                 <p>Acerca del proyecto</p>
                 <p>Blog</p>
                 <p>Contacto</p>
                 <p>Equipo</p>
-                
+
             </div>
         </>
     )
