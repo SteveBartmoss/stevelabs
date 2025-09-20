e31f1fd476ed2fb25c54da3910a90f3a1aaa8cdf
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-09-20 15:16:12 -0600

    feat: Se implemento el contenedor para centrar el contenido

--- src/pages/proyects.jsx
+++ src/pages/proyects.jsx
@@ -1,27 +1,30 @@
+import { DivCenter } from "../components/containers/containers";
 
 
-export function Proyecst(){
-    return(
+export function Proyecst() {
+    return (
         <>
-            <h1>Proyectos OpenSource</h1>
+            <DivCenter>
+                <h1>Proyectos OpenSource</h1>
 
-            <p>
-                Los siguientes proyectos tiene la licencia GNU
-            </p>
+                <p>
+                    Los siguientes proyectos tiene la licencia GNU
+                </p>
+            </DivCenter>
 
             <h1>HttpClient</h1>
 
             <p>
-                Un cliente ligero para probar apis, que no requiere nada mas que la 
-                pila de tecnologias de node que ya se suele usar para desarrollar en 
+                Un cliente ligero para probar apis, que no requiere nada mas que la
+                pila de tecnologias de node que ya se suele usar para desarrollar en
                 frame works como nest, express, react, vue o angular
             </p>
 
             <h1>CronosJs</h1>
 
             <p>
-                Libreria simple para trabajar con fechas en js, aunque no usa 
-                tantas opciones como otras librerias tiene una buena base para 
+                Libreria simple para trabajar con fechas en js, aunque no usa
+                tantas opciones como otras librerias tiene una buena base para
                 manipular fechas en js
             </p>
 
