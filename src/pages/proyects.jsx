29fade4aba8d17d180a86f66de90e933a410c019
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-09-19 22:25:04 -0600

    feat: Se agrego contenido para los proyectos

--- src/pages/proyects.jsx
+++ src/pages/proyects.jsx
@@ -11,6 +11,12 @@ export function Proyecst(){
 
             <h1>HttpClient</h1>
 
+            <p>
+                Un cliente ligero para probar apis, que no requiere nada mas que la 
+                pila de tecnologias de node que ya se suele usar para desarrollar en 
+                frame works como nest, express, react, vue o angular
+            </p>
+
             <h1>CronosJs</h1>
 
             <h1>NeoFetch</h1>
