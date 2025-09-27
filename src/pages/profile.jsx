23c51ef3466419dca7532bb0e1db9e6ccb1cd989
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-09-27 14:54:59 -0600

    feat: Se agrego contenido para el codigo de presentacion

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,29 +1,43 @@
+import { DivCenter } from "../components/containers/containers"
 
 
-export function Profile(){
+export function Profile() {
 
-    const codeStats = 
-    `const steve={
-        lenguajes: [Javascript,php,Java,python],
-        frameWorsk: [Nests,Express,Laravel,Vue],
-        librerias: [React, Redux,Pinia,Jest],
-        dataBase: [Mysql,SlqServer,Informix],
+    const codeStats =
+        `  const steve={
+    code: ["Javascript","php","Java","python"],
+    askMeAbout: ["web dev","tecnologia","pc master race", "videojuegos"],
+    technologies: {
+        frontEnd: ["React","Vue","Laravel"],
+        backEnd: ["Nest","Epress","Laravel"],
+        dataBase: ["Mysql", "SqlServer", "Informix"],
+        state: ["Redux","Pinia"],
+        test: ["Jest"],
+        orm: ["sequelize","typeorm"],
+    },
+    architecture: ["Single page applications"],
+    funFact: "There are two ways to write error-free programs; only the third one works",
+    currentOccupation: ["Full stack developer for a company"],
+    challenge: "LightClient, NeoFetch"
     }`
-    return(
+    
+    return (
         <>
-            <h1>Hola chum, Soy Steve</h1>
+            <DivCenter>
+                <h1>Hola chum, Soy Steve</h1>
 
-            <p>
-                Ingeniero en sistemas al que le gusta crear programas que puedan ayudar a otros o simplemente para aprender sobre algo
-            </p>
+                <p>
+                    Ingeniero en sistemas al que le gusta crear programas que puedan ayudar a otros o simplemente para aprender sobre algo
+                </p>
 
-            <pre>
-                {
-                    codeStats
-                }        
-            </pre>
+                <pre>
+                    {
+                        codeStats
+                    }
+                </pre>
 
-            <h1>Te interesa algun proyecto ? Contacta conmigo</h1>
+                <h1>Te interesa algun proyecto ? Contacta conmigo</h1>
+            </DivCenter>
         </>
     )
 }
