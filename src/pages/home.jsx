35eb20f69ff01bc8f237e9dd3ad1f9a2ea6aef0e
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-09-27 14:54:02 -0600

    feat: Se implemento el div center

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,14 @@
+import { DivCenter } from "../components/containers/containers";
 
 
-export function Home(){
-    return(
+export function Home() {
+    return (
         <>
-            <h1>Home</h1>
+            <DivCenter>
+                <h1>Home</h1>
 
-            <p>Equipo de programacion dedicado a ofrecer software a la medida, opciones de software gratuito, proyectos de codigo abierto y tutoriales</p>
+                <p>Equipo de programacion dedicado a ofrecer software a la medida, opciones de software gratuito, proyectos de codigo abierto y tutoriales</p>
+            </DivCenter>
         </>
     )
 }
