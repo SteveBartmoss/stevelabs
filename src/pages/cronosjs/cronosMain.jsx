66eea6cff1f75222d294c73592bc7de2ca06b545
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-09-27 15:12:33 -0600

    feat: Se implemento el archivo para la pagina principal de cronoskjs:

---
+++ src/pages/cronosjs/cronosMain.jsx
@@ -0,0 +1,21 @@
+
+
+export function CronosMain(){
+    return(
+        <>
+            <h1>Cronos Js</h1>
+
+            <p>
+                Libreria simple que contenie funciones utiles para la manipulacion de fechas en js, 
+                se busca que la libreria te permita usar solo las funciones que requires, de esta forma 
+                es simple manipular fechas.
+            </p>
+
+            <h1>Instalacion</h1>
+
+            <p>
+                La libreria esta disponible en npm por lo que basta con usar el siguiente comando
+            </p>
+        </>
+    )
+}
