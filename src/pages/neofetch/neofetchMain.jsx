0bce3ea7dde056bfb3fa926e967d25658f3b3ae9
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-10-25 10:52:18 -0600

    feat: Se implemento una pagina para le proyecto de neofetch

---
+++ src/pages/neofetch/neofetchMain.jsx
@@ -0,0 +1,19 @@
+
+
+export function NeofetchMain(){
+    return(
+        <>
+            <h1>NeoFetch</h1>
+
+            <p>
+                Wrapper para fetch, que permite realizar las peticiones de una forma mas simple
+            </p>
+
+            <h1>Instalacion</h1>
+
+            <p>
+                La libreria esta disponible en npm por lo que basta con usar el siguiente comando
+            </p>
+        </>
+    )
+}
